a824b6a9298a431784a3ced068ce7321d4ed0dcd
ItBeeZ <probaemail2023@gmail.com>
2026-01-11 15:27:43 +0100

    Backend images loading

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const imageRoutes = require("./routes/imageRoutes");
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/images", imageRoutes);
 
 // Email Configuration
 const transporter = nodemailer.createTransport({
@@ -26,7 +30,6 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// Routes
 app.post("/api/send-email", async (req, res) => {
   const { name, email, message } = req.body;
 
